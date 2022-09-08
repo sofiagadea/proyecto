88a6f5606e24297b6676560ff45c5b5ffcf64ba1
Nicolás Gebauer <80332183+nicolasgebauer@users.noreply.github.com>
2022-09-07 21:08:45 -0300

    reset

--- other/views.py
+++ other/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def simple_view(request):
-    return HttpResponse("HOLA")
+    return HttpResponse("HOLA como estas")
 # Create your views here.
